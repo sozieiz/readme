13ae999cb16ab987208333718b706923b16b8ff5
sozieiz <99080697@mydavinci.nl>
2024-09-06 09:38:21 +0200

    Add files via upload

---
+++ # read me.py
@@ -0,0 +1,4 @@
+# read me
+# Programmeerwerk
+# Eigenlijk een beetje van alles wat je in de bootcamp hebt gezien en gedaan.
+# We beoordelen deze toets dus ook gewoon zoals we dat bij iedere toets doen.
